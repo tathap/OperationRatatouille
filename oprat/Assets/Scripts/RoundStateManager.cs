bd1849de31ec047f389e448e937cb7d39c6ef7e4
Daniel Ng <ng17daniel@gmail.com>
2026-03-15 10:34:21 -0400

    MILK CART

--- oprat/Assets/Scripts/RoundStateManager.cs
+++ oprat/Assets/Scripts/RoundStateManager.cs
@@ -1,4 +1,6 @@
 using System.Collections.Generic;
+using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class RoundStateManager : MonoBehaviour
@@ -11,6 +13,33 @@ public class RoundStateManager : MonoBehaviour
     int qualityScore = 0;
     int varietyScore = 0;
     [SerializeField] List<FoodInstance> cart = new List<FoodInstance>();
+
+    [Header("UI FIELDS")]
+    [SerializeField] TMP_Text weekText;
+    [SerializeField] TMP_Text moneyText;
+    [SerializeField] TMP_Text qualityText;
+    [SerializeField] TMP_Text varietyText;
+
+    public void UpdateUI()
+    {
+        weekText.text = "week: " + (round + 1).ToString();
+        moneyText.text = "money: " + money.ToString();
+        if (totalfood > 0)
+        {
+            qualityText.text = "quality: " + ((float)qualityScore / totalfood).ToString("F2");
+            varietyText.text = "variety: " + ((float)qualityScore / totalfood).ToString("F2");
+        }
+        else
+        {
+            qualityText.text = "N/A";
+            varietyText.text = "N/A";
+        }
+    }
+
+    public void Awake()
+    {
+        minigameManager = FindFirstObjectByType<MinigameManager>();
+    }
 
     public void Start()
     {
@@ -31,11 +60,15 @@ public class RoundStateManager : MonoBehaviour
     /// Main entry point for game playing
     /// </summary>
     /// <param name="config"></param>
-    public void TryPlayGame(MinigameConfig config)
+    public void TryPlayGame(VendorConfig config)
     {
         if (CanAfford(config.cost))
         {
-            minigameManager.EnterGame(config);
+            minigameManager.EnterGame(config.minigameConfig);
+        }
+        else
+        {
+            Debug.Log("cant afford vendor: " + config.Name);
         }
     }
     /// <summary>
