bae7c6879ab9cc0168d600606b7869d91184e362
Daniel Ng <ng17daniel@gmail.com>
2026-03-15 07:09:00 -0400

    maize minigame done

---
+++ oprat/Assets/Scripts/MazeManager.cs
@@ -0,0 +1,219 @@
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class MazeManager : MonoBehaviour
+{
+    FishGameHook gameHook;
+
+    const int BLANK = 0;
+    const int WALL = 1;
+    const int PLAYER = 2;
+    const int GOAL = 3;
+    float timer = 0;
+
+    [SerializeField] GameObject mazeParentSeedPosition;
+    [SerializeField] float stepInterval;
+    const int ROWS = 5;
+    const int COLS = 5;
+    [SerializeField] GameObject wallPrefab;
+    [SerializeField] GameObject goalPrefab;
+    [SerializeField] GameObject playerPrefab;
+    bool canMove = false;
+    GameObject playerInstance;
+    [SerializeField] int[,] mazeState;
+    [SerializeField] int[] playerPos = { 0, 0 };
+
+    List<int[,]> mazes = new List<int[,]>();
+
+    void Awake()
+    {
+        gameHook = FindFirstObjectByType<FishGameHook>();
+    }
+
+    void InitializeMazes()
+    {
+        int[,] maze1 =
+        {
+            {2,0,1,0,0},
+            {1,0,0,0,1},
+            {1,0,1,0,1},
+            {0,0,0,0,0},
+            {0,1,1,1,3}
+        };
+        int[,] maze2 =
+        {
+            {2,0,0,0,0},
+            {0,1,1,0,1},
+            {0,0,0,0,0},
+            {0,1,0,1,1},
+            {0,1,0,0,3}
+        };
+        int[,] maze3 =
+        {
+            {1,0,0,2,0},
+            {0,0,1,1,0},
+            {0,1,3,1,0},
+            {0,0,0,1,0},
+            {1,1,0,0,0}
+        };
+        int[,] maze4 =
+        {
+            {0,0,3,0,0},
+            {1,1,1,1,0},
+            {0,0,0,1,0},
+            {0,1,2,1,0},
+            {0,1,0,0,0}
+        };
+        int[,] maze5 =
+        {
+            {2,0,0,0,0},
+            {0,1,1,0,1},
+            {0,0,1,0,0},
+            {1,0,0,0,0},
+            {1,1,0,1,3}
+        };
+        int[,] maze6 =
+        {
+            {0,0,0,0,0},
+            {0,0,0,0,0},
+            {0,2,1,3,0},
+            {0,0,0,0,0},
+            {0,0,0,0,0}
+        };
+
+        mazes.Add(maze1);
+        mazes.Add(maze2);
+        mazes.Add(maze3);
+        mazes.Add(maze4);
+        mazes.Add(maze5);
+        mazes.Add(maze6);
+    }
+
+    public void GenerateMaze(int[,] maze)
+    {
+        for (int i = 0; i < ROWS; i++)
+        {
+            for (int j = 0; j < COLS; j++)
+            {
+                print(i + ", " + j + " - " + ROWS);
+
+                Vector3 position = (
+                    mazeParentSeedPosition.transform.position
+                    + new Vector3(j * stepInterval, 0, 0)
+                    + new Vector3(0, i * stepInterval, 0)
+                );
+                int value = maze[i, j];
+                GameObject myPrefab = null;
+                if (value == 1) myPrefab = wallPrefab;
+                if (value == 2) myPrefab = playerPrefab;
+                if (value == 3) myPrefab = goalPrefab;
+                if (myPrefab == null)
+                {
+                    continue;
+                }
+
+                GameObject myObj = Instantiate(myPrefab, position, Quaternion.identity);
+                myObj.transform.parent = mazeParentSeedPosition.transform;
+                if (value == PLAYER)
+                {
+                    playerInstance = myObj;
+                    playerPos[0] = i;
+                    playerPos[1] = j;
+                }
+            }
+        }
+    }
+    public void TryMove(Vector2 inputVector)
+    {
+        int newY = (int)inputVector.y + playerPos[0];
+        if (newY < 0 || newY >= ROWS) return;
+        int newX = (int)inputVector.x + playerPos[1];
+        if (newX < 0 || newX >= COLS) return;
+
+        if (mazeState[newY, newX] == WALL)
+        {
+            return;
+        }
+        if (mazeState[newY, newX] == GOAL)
+        {
+            EndGame();
+            return;
+        }
+        mazeState[playerPos[0], playerPos[1]] = 0;
+        mazeState[newY, newX] = PLAYER;
+
+        playerPos[0] = newY;
+        playerPos[1] = newX;
+
+        playerInstance.transform.position = (
+            mazeParentSeedPosition.transform.position
+            + new Vector3(playerPos[1] * stepInterval, 0, 0)
+            + new Vector3(0, playerPos[0] * stepInterval, 0)
+        );
+    }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        InitializeMazes();
+        mazeState = mazes[Random.Range(0, 6)];
+        print(mazeState);
+        GenerateMaze(mazeState);
+        canMove = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!canMove) return;
+
+        timer += Time.deltaTime;
+
+        if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            TryMove(Vector2.up);
+        }
+        else if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            TryMove(Vector2.left);
+        }
+        else if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            TryMove(Vector2.down);
+        }
+        else if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            TryMove(Vector2.right);
+        }
+    }
+
+    int ComputeScore(float timer)
+    {
+        if (timer < 2f)
+        {
+            return 5;
+        }
+        else if (timer < 3f)
+        {
+            return 4;
+        }
+        else if (timer < 4f)
+        {
+            return 3;
+        }
+        else if (timer < 5f)
+        {
+            return 2;
+        }
+        else
+        {
+            return 1;
+        }
+    }
+    public void EndGame()
+    {
+        int score = ComputeScore(timer);
+        gameHook.HandleGameEnd(score);
+    }
+}
